68f3084bc0bc86ddfcbc62c990ea3477197278f5
James <jamesdev4you@gmail.com>
2023-11-02 12:30:34 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from '@mui/material/styles';
+import './index.css';
+import Typography from '@mui/material/Typography';
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FEFAE0',
+    },
+    secondary: {
+      main: '#D4A373',
+    },
+    error: {
+      main: '#FAEDCD',
+    },
+  },
+  typography: {
+    h1: {
+      fontFamily: 'KaushanScript',
+    },
+    h2: {
+      fontFamily: 'KaushanScript',
+    },
+    h3: {
+      fontFamily: 'KatibehRegular',
+    },
+    h4: {
+      fontFamily: 'KatibehRegular',
+    },
+    h5: {
+      fontFamily: 'KatibehRegular',
+    },
+    h6: {
+      fontFamily: 'KatibehRegular',
+    },
+    subtitle1: {
+      fontFamily: 'KatibehRegular',
+    },
+    subtitle2: {
+      fontFamily: 'KatibehRegular',
+    },
+    body1: {
+      fontFamily: 'KatibehRegular',
+    },
+    body2: {
+      fontFamily: 'KatibehRegular',
+    },
+  },
+});
+
+theme = responsiveFontSizes(theme, {
+  breakpoints: ['xs', 'sm', 'md', 'lg', 'xl'],
+  factor: 2,
+  variants: [
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'subtitle1',
+    'subtitle2',
+    'body1',
+    'body2',
+  ],
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Typography variant='h3'>YO</Typography>
+      </div>
+    </ThemeProvider>
   );
 }
 
